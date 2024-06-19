158c7d1f1e30b561f529b290f781d0e6e3c5c046
Alex Coleman <alex.coleman@activetravelengland.gov.uk>
2024-06-19 07:55:27 +0100

    Add e2e test for click behaviour

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -6,3 +6,11 @@ test('index shows title', async ({ page }) => {
 
   await expect(page.getByRole("heading", { name: "Simple JS"})).toBeVisible()
 });
+
+test('index click shows clicked', async ({ page }) => {
+  await page.goto('/');
+
+  await page.getByRole("button").click()
+
+  await expect(page.getByRole("paragraph")).toHaveText("Clicked!")
+});
